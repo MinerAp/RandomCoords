8a7ae0baa7448df6a4aad74ee2e8076daa15cec9
Andrew Shulman <andy.shulman@hotmail.com>
2014-09-03 17:21:51 -0400

    Start the player close to 0, 0

--- src/main/java/randomcoords/RandomOffset.java
+++ src/main/java/randomcoords/RandomOffset.java
@@ -9,22 +9,16 @@ public class RandomOffset {
     private static Map<String, Offset<Integer>> xz = new ConcurrentHashMap<String, Offset<Integer>>();
 
     public static void generateOffset(Player player) {
-        int n = 65536;
+        int x = -player.getLocation().getBlockX();
+        int z = -player.getLocation().getBlockZ();
 
-        int min_x = -n - player.getLocation().getBlockX() / 16;
-        int max_x = n - player.getLocation().getBlockX() / 16;
-        int min_z = -n - player.getLocation().getBlockZ() / 16;
-        int max_z = n - player.getLocation().getBlockZ() / 16;
-
-        int x = min_x + (int) (Math.random() * (max_x - min_x + 1));
-        int z = min_z + (int) (Math.random() * (max_z - min_z + 1));
-
-        x *= 16;
-        z *= 16;
         if (player.hasPermission("randomcoords.bypass")) {
             x = 0;
             z = 0;
         }
+
+        x -= x % 16;
+        z -= z % 16;
 
         xz.put(player.getName(), new Offset<Integer>(x, z));
     }
